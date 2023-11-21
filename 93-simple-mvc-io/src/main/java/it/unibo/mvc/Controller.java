110b030f2f2d3483dde62f8cd161d5e1e1400b94
Alessandro Ronchi <alessandro.ronchi7@studio.unibo.it>
2023-11-21 12:20:29 +0100

    Implemented part 1 and 2 of exercise 93 (CASA)

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,63 @@
 package it.unibo.mvc;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    private static final String DEFAULT_PATH = System.getProperty("user.home") +
+        System.getProperty("file.separator") + "output.txt";
+
+    private File currentFile;
+
+    public Controller() {
+        this.currentFile = new File(DEFAULT_PATH);
+    }
+
+    /**
+     * Sets the parameter as the current File
+     * @param file
+     */
+    public void setCurrentFile(final File file) {
+        this.currentFile = file;
+    }
+
+    /**
+     * Returns the current File
+     * @return the current File
+     */
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    /**
+     * Returns the absolute path of the current File
+     * @return a String containing the absolute path of the File
+     */
+    public String getFilePath() {
+        return this.currentFile.getAbsolutePath();
+    }
+
+    /**
+     * Gets a String as input and writes its content on the current File
+     * @param content
+     * @throws IOException
+     */
+    public void writeOnFile(String content) throws IOException {
+        try(BufferedWriter os = new BufferedWriter(
+            new OutputStreamWriter(
+                new FileOutputStream(this.currentFile.getAbsolutePath()), StandardCharsets.UTF_8))) {
+            os.write(content);
+        } catch(final IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,17 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +19,48 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 3;
+    private static final String TITLE = "Write on file GUI";
+
+    private final JFrame frame = new JFrame(TITLE);
+    private final Controller controller = new Controller();
+
+    public SimpleGUI() {
+        JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        JTextArea text = new JTextArea();
+        JButton save = new JButton("Save");
+        canvas.add(text, BorderLayout.CENTER);
+        canvas.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        save.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    controller.writeOnFile(text.getText());
+                } catch (final IOException e1) {
+                    JOptionPane.showMessageDialog(frame, e1, "Write Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sWidth = (int) screen.getWidth();
+        final int sHeight = (int) screen.getHeight();
+        frame.setSize(sWidth/PROPORTION, sHeight/PROPORTION);
+
+        frame.setLocationByPlatform(true);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(final String[] args) {
+        new SimpleGUI().display();
+    }
 
 }
