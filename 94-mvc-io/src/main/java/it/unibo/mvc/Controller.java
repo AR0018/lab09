c208e1bc39f0e4b9b60b2d83920da575a131afde
Alessandro Ronchi <alessandro.ronchi7@studio.unibo.it>
2023-11-24 17:42:11 +0100

    Implemented SimpleController in exercise 94 (CASA)

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,38 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ *Interface that models a simple controller responsible of I/O access
  */
 public interface Controller {
+
+    /**
+     * Sets the next string to print
+     * 
+     * @param newString
+     * @throws NullPointerException if the string passed is null
+     */
+    void setNextString(String newString);
+
+    /**
+     * @return the next string to print, or null if the string is unset
+     */
+    String getNextString();
+
+    /**
+     * Returns the history of the printed strings
+     * 
+     * @return a list with every printed string
+     */
+    List<String> getHistory();
+
+    /**
+     * Prints the current string
+     * 
+     * @throws IllegalStateException if the current string is unset
+     */
+    void printString();
+
 
 }
--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,43 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 /**
- * 
+ * Implementation of a simple controller for I/O access
  *
  */
 public final class SimpleController implements Controller {
+
+    private String current;
+    private final List<String> history = new ArrayList<>();
+
+    @Override
+    public void setNextString(final String newString) {
+        Objects.requireNonNull(newString);
+        this.current = newString;
+    }
+
+    @Override
+    public String getNextString() {
+        return this.current;
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return Collections.unmodifiableList(history);
+    }
+
+    @Override
+    public void printString() {
+        if(this.current == null) {
+            throw new IllegalStateException("The current string is unset");
+        }
+        System.out.println(current);
+        history.add(current);
+        current = null;
+    }
 
 }
